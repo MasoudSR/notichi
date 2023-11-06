83ff95aebfa897f29e25968c0ca9e238db147e9a
Masoud S.R <amir.sepehr72@gmail.com>
2023-11-06 12:19:29 +0330

    header added

---
+++ src/components/template/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="flex justify-between px-6 py-4 m-4 bg-amber-200 border-r-3 rounded-lg">
+			<span>Notichi</span>
+			<span>Search</span>
+		</div>
+  )
+}

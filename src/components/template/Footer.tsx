e21d1bc45869e30e20e776dc2e7d2bc2d80fe845
Masoud S.R <amir.sepehr72@gmail.com>
2023-11-07 09:31:35 +0330

    footer added

--- src/components/template/Footer.tsx
+++ src/components/template/Footer.tsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div className="mt-auto flex justify-between ">
+			<button className="p-4 m-3 bg-gray-400 rounded-lg">All</button>
+			<button className="p-4 m-3 bg-gray-400 rounded-lg">Folders</button>
+			<button className="p-4 m-3 bg-gray-400 rounded-lg">Settings</button>
+		</div>
+	);
 }
--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Header() {
   return (
     <div className="flex justify-between px-6 py-4 m-4 bg-amber-200 border-r-3 rounded-lg">
-			<span>Notichi</span>
-			<span>Search</span>
+      <span>Search</span>
+			<span className='absolute left-1/2 -translate-x-1/2'>Notichi</span>
+			<span>Add</span>
 		</div>
   )
 }

9ca3d6947ef7f1ec84e26f8d36ab5049d3b1e716
Masoud S.R <amir.sepehr72@gmail.com>
2023-11-08 18:01:20 +0330

    link on margins bug fixed

--- src/components/module/Note.tsx
+++ src/components/module/Note.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Note({ id, title, note }: { id: string; title: string; note: string }) {
 	return (
-		<Link href={`/note/${id}`}>
-			<div className="bg-white rounded-lg m-6 drop-shadow">
+		<div className="bg-white rounded-lg m-6 drop-shadow">
+			<Link href={`/note/${id}`}>
 				<h4 className="text-xl font-bold p-5">{title}</h4>
 				<p className="border-t-2 border-[#f2f2f7] p-5">{note}</p>
-			</div>
-		</Link>
+			</Link>
+		</div>
 	);
 }

fce59b8bfb32c1bc77359a5af4b6778f7a1772a1
Masoud S.R <amir.sepehr72@gmail.com>
2023-11-10 12:43:36 +0330

    header icons and back button improved

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,25 +1,29 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { BsPlusSquare } from "react-icons/bs";
-import { IoChevronBackOutline } from "react-icons/io5";
+import { usePathname, useRouter } from "next/navigation";
+import { CgFileAdd } from "react-icons/cg";
+import { BiArrowBack } from "react-icons/bi";
 
 export default function Header() {
 	const pathName = usePathname();
+	const router = useRouter();
+	const pathCheck = () => {
+		const paths = ["/", "/folders", "/settings"];
+		return paths.includes(pathName);
+	};
 	return (
-		<div className="flex justify-between font-bold items-center align-middle px-8 py-5 bg-amber-200 rounded-b-xl sticky top-0 drop-shadow z-10 backdrop-blur-sm">
+		<div className="flex justify-between font-bold items-center align-middle pl-12 pr-9 py-5 bg-amber-200 rounded-b-xl sticky top-0 drop-shadow z-10 backdrop-blur-sm">
 			<div className="flex items-center">
-				{pathName !== "/" && (
-					<Link href="/">
-						<IoChevronBackOutline size={25} />
-					</Link>
+				{!pathCheck() && (
+					<button className="absolute left-3" onClick={router.back}>
+						<BiArrowBack size={27} />
+					</button>
 				)}
-
 				<span className="text-3xl">Notichi</span>
 			</div>
 			<span>
 				<Link href="/add" className="flex items-center">
-					<BsPlusSquare size={25} />
+					<CgFileAdd size={27} />
 				</Link>
 			</span>
 		</div>

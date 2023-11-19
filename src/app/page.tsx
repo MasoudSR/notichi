6d37aefd9eb057d78ac520c78bf6721e3df02847
Masoud S.R <amir.sepehr72@gmail.com>
2023-11-19 10:28:13 +0330

    feat : search between notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,41 @@
 
 import Note from "@/components/module/Note";
 import loadStorage from "@/helpers/loadStorage";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+
+type DataType = {
+	updatedAt: string | Date;
+	notes: { id: string; title: string; text: string; folderId: string; folderName: string }[];
+	folders: { id: string; name: string; notesId: string[] }[];
+};
+type NoteType = { id: string; title: string; text: string; folderId: string; folderName: string };
 
 export default function Home() {
-	const [notes, setNotes] = useState<
-		{ id: string; title: string; text: string; folderId: string; folderName: string }[]
-	>([]);
+	const [notes, setNotes] = useState<NoteType[]>([]);
+	const [data, setData] = useState<DataType>();
+
 	useEffect(() => {
-		const data = loadStorage();
-		setNotes(data.notes);
+		const storageData = loadStorage();
+		setData(storageData);
+		setNotes(storageData.notes);
 	}, []);
+
+	const searchHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+		const value = e.target.value;
+		const filteredNotes = data!.notes.filter((note) => note.title.includes(value) || note.text.includes(value));
+		setNotes(filteredNotes);
+	};
+
 	return (
-		<main>
+		<main className="flex flex-col">
+			<div className="mx-6 mt-6">
+				<input
+					type="text"
+					className="bg-[#E3E3E8] rounded-lg w-full p-3"
+					placeholder="Search"
+					onChange={searchHandler}
+				/>
+			</div>
 			{notes.map((note: { id: string; title: string; text: string }) => (
 				<Note key={note.id} id={note.id} title={note.title} text={note.text} />
 			))}

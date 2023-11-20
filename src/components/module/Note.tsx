061ceaf5ed9c3828701230db8a0fa5a66d095778
Masoud S.R <amir.sepehr72@gmail.com>
2023-11-20 10:54:48 +0330

    note text only top border bug fixed

--- src/components/module/Note.tsx
+++ src/components/module/Note.tsx
@@ -5,7 +5,7 @@ export default function Note({ id, title, text }: { id: string; title: string; t
 		<div className="bg-white rounded-lg m-6 drop-shadow">
 			<Link href={`/note/${id}`}>
 				{title !== "" && <h4 className="text-xl font-bold p-5">{title}</h4>}
-				{text !== "" && <p className="border-t-2 border-[#f2f2f7] p-5 whitespace-pre-line truncate">{text}</p>}
+				{text !== "" && <p className={`${title !== "" && "border-t-2"} border-[#f2f2f7] p-5 whitespace-pre-line truncate`}>{text}</p>}
 			</Link>
 		</div>
 	);

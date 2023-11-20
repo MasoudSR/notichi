9d67de666c132f71363b4cd36c006370c4432797
Masoud S.R <amir.sepehr72@gmail.com>
2023-11-20 11:59:19 +0330

    loading page added

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+	return (
+		<div className="flex items-center mt-24 flex-col">
+			<span className="border-8 border-t-orange-300 animate-spin h-14 w-14 mr-3 block rounded-full" />
+		</div>
+	);
+}

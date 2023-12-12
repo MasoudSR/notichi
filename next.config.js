f6fdff068fa03ce8536a9cac5c93777861800bf6
Masoud S.R <amir.sepehr72@gmail.com>
2023-12-12 13:26:30 +0330

    google acc image not showing problem fixed

--- next.config.js
+++ next.config.js
@@ -6,7 +6,15 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {images:{
-	domains:["avatars.githubusercontent.com"]
+	domains:["avatars.githubusercontent.com"],
+	remotePatterns:[
+		{
+			protocol:"https",
+			hostname:"*.googleusercontent.com",
+			port:"",
+			pathname:"**",
+		}
+	]
 }};
 
 module.exports = withPWA(nextConfig);

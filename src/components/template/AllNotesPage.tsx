2560f68ba5ae3f8d6139698f2daf41671ba75b66
Masoud S.R <amir.sepehr72@gmail.com>
2024-03-15 18:20:32 +0330

    If the searched text has no results, the appropriate text will be displayed

--- src/components/template/AllNotesPage.tsx
+++ src/components/template/AllNotesPage.tsx
@@ -5,6 +5,7 @@ import Note from "@/components/module/Note";
 import loadStorage from "@/helpers/loadStorage";
 import { loadSettings } from "@/helpers/settingsManager";
 import React, { useContext, useEffect, useState } from "react";
+import { CgFileAdd } from "react-icons/cg";
 
 type DataType = {
 	updatedAt: string | Date;
@@ -24,44 +25,59 @@ export default function AllNotesPage() {
 	const [notes, setNotes] = useState<NoteType[]>([]);
 	const [data, setData] = useState<DataType>();
 	const { isMounted, setIsMounted, prevPageName, selectedPageName } = useContext(Context);
-	const [settings , setSettings] = useState({animations:true})
+	const [settings, setSettings] = useState({ animations: true });
+	const [search, setSearch] = useState("");
 
 	useEffect(() => {
 		const storageData = loadStorage();
 		setData(storageData);
 		setNotes(storageData.notes);
 		setIsMounted(true);
-		setSettings(loadSettings)
+		setSettings(loadSettings);
 	}, []);
 
 	const searchHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
 		const value = e.target.value;
+		setSearch(value);
 		const filteredNotes = data!.notes.filter((note) => note.title.includes(value) || note.text.includes(value));
 		setNotes(filteredNotes);
 	};
 
-	const animations = settings.animations ? isMounted
-				? prevPageName.name === "folders" || prevPageName.name === "settings"
-					? "animate-fade-right animate-duration-150 animate-ease-out"
-					: "animate-fade-up animate-duration-150 animate-ease-out"
-				: selectedPageName === "folders" || selectedPageName === "settings"
-				? "animate-fade-out-left animate-duration-150 animate-ease-out"
-				: "animate-fade-down animate-duration-150 animate-ease-out animate-reverse" : ""
-			
+	const animations = settings.animations
+		? isMounted
+			? prevPageName.name === "folders" || prevPageName.name === "settings"
+				? "animate-fade-right animate-duration-150 animate-ease-out"
+				: "animate-fade-up animate-duration-150 animate-ease-out"
+			: selectedPageName === "folders" || selectedPageName === "settings"
+			? "animate-fade-out-left animate-duration-150 animate-ease-out"
+			: "animate-fade-down animate-duration-150 animate-ease-out animate-reverse"
+		: "";
+
 	return (
-		<main
-			className={`flex flex-col ${animations}`}>
+		<main className={`flex flex-col ${animations}`}>
 			<div className="mx-6 mt-6">
 				<input
 					type="text"
 					className="bg-[#E3E3E8] rounded-lg w-full p-3"
 					placeholder="Search"
+					value={search}
 					onChange={searchHandler}
 				/>
 			</div>
-			{notes.map((note: { id: string; title: string; text: string }) => (
-				<Note key={note.id} id={note.id} title={note.title} text={note.text} />
-			))}
+			{notes.length > 0 ? (
+				notes.map((note: { id: string; title: string; text: string }) => (
+					<Note key={note.id} id={note.id} title={note.title} text={note.text} />
+				))
+			) : search.length > 0 ? (
+				<p className="flex justify-center mt-7">There are no matching results for: {search}</p>
+			) : (
+				<p className="flex flex-col items-center justify-center mt-7">
+					<div>You have no notes</div>
+					<div className="flex justify-center items-center gap-1">
+						Use <CgFileAdd /> button to add new note.
+					</div>
+				</p>
+			)}
 		</main>
 	);
 }
